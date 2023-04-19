bd8a75678a2abfac1a479aa985673993ccde44d4
Ghanshyam Garad Patil <99329031+Garad12@users.noreply.github.com>
2023-04-19 15:28:42 +0530

    Create interview_questions.py

---
+++ interview_questions.py
@@ -0,0 +1,37 @@
+Q.1  What is different between List and Tuple?
+Q.2  What is dictionary in Python?
+Q.3  What are the built-in type does python provides?
+Q.4  What Is A Tuple In Python?
+Q.5  What are types of Operator are used in Python? 
+Q.6  What Is A Built-In Function That Python Uses To Iterate Over A Number Sequence?
+Q.7  Which numbers are printed? 
+Q.8  What is pass in Python?
+Q.9  What Does The Continue Do In Python? 
+Q.10 When Should You Use The “Break” In Python? 
+Q.11 What Is The Difference Between Pass And Continue In Python? 
+Q.12 What is the difference between a for loop and a while loop?
+Q.13 Python program to get the Fibonacci series between 0 to 50.
+Q.14 Python program that accepts a string and calculates the number of digits and letters.
+Q.15 Python program to display numbers from a list using a for loop.
+Q.16 Write a program to check whether a person is eligible for voting or not.
+Q.17 Write a program to check whether a number entered by user is even or odd
+Q.18 Write a program to create function func1() to accept a variable length of arguments and print their value.
+    given
+     # call function with 3 argument
+     func1(20, 40, 60)
+     # call function with 2 arguments
+     func1(80, 100)
+    
+     expected Output:
+      Printing values
+      20
+      40
+      60
+      Printing values
+      80
+      100
+Q.19 Generate a Python list of all the even numbers between 4 to 30
+     Expected Output:
+     [4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28]
+Q.20  Find the largest item from a given list
+      x = [4, 6, 8, 24, 12, 2]
